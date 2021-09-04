fa95e3f5cd56fb222afa4fcacc882a6b921bf735
bernhard@hensler.net <bernhard@hensler.net>
2021-09-04 19:23:12 +0200

    ok

--- test.sh
+++ test.sh
@@ -7,4 +7,8 @@ echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
 echo "AGENT_BUILDDIRECTORY contents:"
 ls -1 $AGENT_BUILDDIRECTORY
 echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
+
+echo "SourceFolder is $System.DefaultWorkingDirectory"
+echo "TargetFolder is $Build.ArtifactStagingDirectory"
+
 echo "Over and out."
